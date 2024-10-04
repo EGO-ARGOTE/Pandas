1dd198345a201a7d7d67ecff1921ec4a7b85c954
EGO <ego.argote@hotmail.com>
2024-10-04 18:12:35 -0500

    Se creo y se ignoro el entorno virtual, y se instalo pandas

--- sesion_1/series_Dataframes.py
+++ sesion_1/series_Dataframes.py
@@ -4,6 +4,7 @@ psg_players = pd.Series (
   ['Navas', 'Mbappe', 'Neymar', 'Messi'],
   index = [1, 7, 10, 30],
 )
+
 psg_dict = {1: 'Navas', 7: 'Mbappe', 10: 'Neymar', 30: 'Messi'}
 psg_players_dict = pd.Series(psg_dict)
 print(psg_players_dict)
